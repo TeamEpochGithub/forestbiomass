c5625f3e071f95fa9093c1755f627bbf51563118
Adrian <a.s.kuiper@student.tudelft.nl>
2022-11-11 17:08:55 +0100

    added fake data for s1 and s2 divided

--- test/create_fake_data.py
+++ test/create_fake_data.py
@@ -9,12 +9,23 @@ def gen_fake_patch_id():
 for _ in range(64):
     patch = gen_fake_patch_id()
 
-    os.mkdir(f"../data/imgs/fake_data/{patch}")
+    os.mkdir(f"../data/imgs/fake_data2/{patch}")
 
+    # Creating fake data while merging the s1 and s2 bands together so 4+11 = 15
+    # for month in range(12):
+    #     file_name = f"../data/imgs/fake_data/{patch}/{patch}_{month}.npy"
+    #     file_name_label = f"../data/imgs/fake_data/{patch}/label.npy"
+    #     fake_array = np.ones((15, 256, 256)) * random.randint(0, 255)
+    #     fake_label = np.ones((256, 256)) * random.randint(0, 255)
+    #     np.save(file_name, fake_array)
+    #     np.save(file_name_label, fake_label)
+
+    #Creating separate arrays for both s1 and s2
     for month in range(12):
-        file_name = f"../data/imgs/fake_data/{patch}/{patch}_{month}.npy"
-        file_name_label = f"../data/imgs/fake_data/{patch}/label.npy"
-        fake_array = np.ones((15, 256, 256)) * random.randint(0, 255)
+        file_name = f"../data/imgs/fake_data2/{patch}/{patch}_{month}.npy"
+        file_name_label = f"../data/imgs/fake_data2/{patch}/label.npy"
+        fake_array_s1 = np.ones((4, 256, 256)) * random.randint(0, 255)
+        fake_array_s2 = np.ones((11, 256, 256)) * random.randint(0, 255)
         fake_label = np.ones((256, 256)) * random.randint(0, 255)
-        np.save(file_name, fake_array)
+        np.save(file_name, [fake_array_s1, fake_array_s2])
         np.save(file_name_label, fake_label)
