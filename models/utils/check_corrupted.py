acdf3b6456c9ec69d9a7bb496cde58f6443e59e1
Philip Tempelman <tempelmanphilip@gmail.com>
2022-12-02 12:13:57 +0100

    add comments and clean up code

--- models/utils/check_corrupted.py
+++ models/utils/check_corrupted.py
@@ -1,9 +1,10 @@
 from collections import Counter
 
-from analysis.exploratory_data_analysis.utils.visual_utils import plot_patch_data
-
 
 def create_coordinates():
+    """
+    Returns 9 evenly spaced horizontal coordinate lines across a 256, 256 image
+    """
     center_coordinates = [(int(256 / 9) * x) - 13 for x in range(1, 10, 3)]
 
     left = [center_coordinates[0] + x for x in range(1, 26)]
@@ -12,11 +13,15 @@ def create_coordinates():
 
     all_horizontal = left + middle + right
     combinations = [(x, y) for y in center_coordinates for x in all_horizontal]
+
     combinations_chunks = [combinations[x:x + 25] for x in range(0, len(combinations), 25)]
     return combinations_chunks
 
 
 def is_corrupted(band):
+    """
+    Returns if band is corrupted by checking if pixels in >= 3 out of 9 lines are all the same value
+    """
     chunk_coordinates = create_coordinates()
 
     corrupted_count = 0
@@ -33,6 +38,4 @@ def is_corrupted(band):
         if pixel_counts[0][1] >= threshold:
             corrupted_count += 1
 
-    if corrupted_count >= 3:
-        # plot_patch_data(band)
-        return True
+    return corrupted_count >= 3
