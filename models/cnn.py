2939fe33861cab4148095ed431320810fcac6d8c
Adrian <kuiper2000@hotmail.com>
2022-12-07 16:59:56 +0100

    cnn with npys

--- models/cnn.py
+++ models/cnn.py
@@ -89,22 +89,77 @@ class DataGeneratorTif(tf.keras.utils.Sequence):
 
         return np.asarray(batch_x), np.asarray(batch_y)
 
+class DataGeneratorNpy(tf.keras.utils.Sequence):
+    def __init__(self, patch_ids, batch_size=4):
+        """
+        Few things to mention:
+            - The data generator tells our model how to fetch one batch of training data (in this case from files)
+            - Any work that can be done before training, should be done in init, since we want fetching a batch to be fast
+            - Therefore, we want all filenames and labels to be determined before training
+            - This saves work, because we will be fetching batches multiple times (across epochs)
+        """
+        self.dir_path = r'C:\Users\kuipe\OneDrive\Bureaublad\Epoch\forestbiomass\data\test/'
+        # Get all filenames in directory
+        self.patches = patch_ids
+        # Include batch size as attribute
+        self.batch_size = batch_size
+
+    def __len__(self):
+        """
+        Should return the number of BATCHES the generator can retrieve (partial batch at end counts as well)
+        """
+        return int(np.ceil(len(self.patches) / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        """
+        Tells generator how to retrieve BATCH idx
+        """
+
+        # Get filenames for X batch
+        batch_patches = self.patches[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_labels = [self.dir_path + p + '/label.npy' for p in batch_patches]
+
+        batch_x = []
+        batch_y = []
+        for label_npy in batch_labels:
+            label = np.load(label_npy).reshape(256 * 256)
+            batch_y.append(label)
+
+        #For batch in patches moet hier nog bij
+        for p in batch_patches:
+            s2_patch = []
+            for month in range(12):
+                s2_month = []
+                try:
+                    bands = [osp.join(self.dir_path + p + f'/{month}/S2/', file) for file in os.listdir(self.dir_path + p + f'/{month}/S2')]
+                except:
+                    continue
+                for band in bands:
+                    s2_month.append(np.load(band))
+
+                s2_patch.append(s2_month)
+            average = np.average(s2_patch, axis=0)
+            average = average.reshape(256, 256, 11)
+            batch_x.append(average)
+
+        return np.asarray(batch_x), np.asarray(batch_y)
+
 if __name__ == '__main__':
     # Change this to some pretrained cnn of keras
     # Check this link for different models: https://www.tensorflow.org/api_docs/python/tf/keras/applications
-    pretrained_cnn = tf.keras.applications.efficientnet.EfficientNetB0(include_top=False,
+    pretrained_cnn = tf.keras.applications.resnet50.ResNet50(include_top=False,
                                                             weights='imagenet',
                                                             input_shape=(256, 256, 3))
     model = CNN(pretrained_cnn)
     model = model.build_graph()
     model.compile(optimizer="adam", loss=tf.keras.losses.MeanSquaredError(),
-                  metrics=[tf.keras.metrics.MeanSquaredError()])
+                  metrics=[tf.keras.metrics.RootMeanSquaredError()])
 
     with open(osp.join(osp.dirname(data.__file__), 'patch_name_test'), newline='') as f:
         reader = csv.reader(f)
         patch_name_data = list(reader)
     patch_names = patch_name_data[0]
 
-    datagen = DataGeneratorTif(patch_names)
+    datagen = DataGeneratorNpy(patch_names)
 
     model.fit(datagen, epochs=100)
