1a0fc05a461bfe5f38eaa771f610ac4f2dda1892
Adrian <a.s.kuiper@student.tudelft.nl>
2022-12-19 17:32:29 +0100

    cnn normalization and gpu run

--- models/cnn.py
+++ models/cnn.py
@@ -6,11 +6,14 @@ import os
 import os.path as osp
 import csv
 import data
-
-
+from PIL import Image
+import data.test_agbm as submission
+import sys
 class CNNModel(tf.keras.Model):
     def __init__(self, base, weights):
         super(CNNModel, self).__init__()
+        # self.inlayer = tf.keras.Input(shape=(256, 256, 11))
+        self.preprocessing = tf.keras.layers.Normalization()
         self.base = base
         base_weights = weights
 
@@ -23,6 +26,7 @@ class CNNModel(tf.keras.Model):
         self.out = tf.keras.layers.Dense(256 * 256, activation='linear')
 
     def call(self, x, training=None, **kwargs):
+        x = self.preprocessing(x)
         x = self.base(x)
         x = self.pool(x)
         x = self.dens3(x)
@@ -33,21 +37,19 @@ class CNNModel(tf.keras.Model):
 def create_model(base_model, base_weights):
     model = CNNModel(base_model, base_weights)
     model.build(input_shape=(None, 256, 256, 11))
-    model.compile(optimizer="adam", loss=tf.keras.losses.MeanSquaredError(),
-                  metrics=[tf.keras.metrics.RootMeanSquaredError()])
 
     return model
 
 
-def get_patch_names():
-    with open(osp.join(osp.dirname(data.__file__), 'patch_names'), newline='') as f:
+def get_patch_names(file_name):
+    with open(osp.join(osp.dirname(data.__file__), file_name), newline='') as f:
         reader = csv.reader(f)
         patch_name_data = list(reader)
 
     return patch_name_data[0]
 
 class DataGeneratorTif(tf.keras.utils.Sequence):
-    def __init__(self, patch_ids, batch_size=4):
+    def __init__(self, patch_ids, batch_size=32):
         """
         Few things to mention:
             - This data generator uses original tif files, now only s2 and averages them for every patch
@@ -105,7 +107,7 @@ class DataGeneratorTif(tf.keras.utils.Sequence):
         return np.asarray(batch_x), np.asarray(batch_y)
 
 class DataGeneratorNpy(tf.keras.utils.Sequence):
-    def __init__(self, patch_ids, batch_size=4):
+    def __init__(self, patch_ids, batch_size=32):
         """
         Few things to mention:
             - The data generator tells our model how to fetch one batch of training data (in this case from files)
@@ -153,6 +155,63 @@ class DataGeneratorNpy(tf.keras.utils.Sequence):
                     bands = [osp.join(self.dir_path + p + f'/{month}/S2/', file) for file in os.listdir(self.dir_path + p + f'/{month}/S2')]
                     for band in bands:
                         s2_month.append(np.load(band))
+                    # s2_month = tf.image.per_image_standardization(np.asarray(s2_month))
+                    s2_patch.append(s2_month)
+                except:
+                    continue
+
+            # average all the bands per patch together.
+            average = np.average(s2_patch, axis=0)
+            # Reshape it so that the CNN model can take the data in. 11 is the number of channels.
+            # average = tf.image.per_image_standardization(average).numpy()
+            average = average.reshape(256, 256, 11)
+            batch_x.append(average)
+
+        return np.asarray(batch_x), np.asarray(batch_y)
+
+class DataGeneratorPredict(tf.keras.utils.Sequence):
+    def __init__(self, patch_ids, batch_size=32):
+        """
+        Few things to mention:
+            - The data generator tells our model how to fetch one batch of training data (in this case from files)
+            - Any work that can be done before training, should be done in init, since we want fetching a batch to be fast
+            - Therefore, we want all filenames and labels to be determined before training
+            - This saves work, because we will be fetching batches multiple times (across epochs)
+        """
+        self.dir_path = r'C:\Users\kuipe\Desktop\Epoch\forestbiomass\data\testing_converted/'
+        # Get all filenames in directory
+        self.patches = patch_ids
+        # Include batch size as attribute
+        self.batch_size = batch_size
+
+    def __len__(self):
+        """
+        Should return the number of BATCHES the generator can retrieve (partial batch at end counts as well)
+        """
+        return int(np.ceil(len(self.patches) / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        """
+        Tells generator how to retrieve BATCH idx
+        """
+
+        # Get filenames for X batch
+        batch_patches = self.patches[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        batch_x = []
+
+        # Get all possible s2 bands that are available per patch
+        for p in batch_patches:
+
+            s2_patch = []
+            for month in range(12):
+                s2_month = []
+
+                # Check if S2 data exists for a specific month
+                try:
+                    bands = [osp.join(self.dir_path + p + f'/{month}/S2/', file) for file in os.listdir(self.dir_path + p + f'/{month}/S2')]
+                    for band in bands:
+                        s2_month.append(np.load(band))
                 except:
                     continue
 
@@ -164,26 +223,51 @@ class DataGeneratorNpy(tf.keras.utils.Sequence):
             average = average.reshape(256, 256, 11)
             batch_x.append(average)
 
-        return np.asarray(batch_x), np.asarray(batch_y)
+        return np.asarray(batch_x)
+
+def create_submissions():
+    submission_path = r'C:\Users\kuipe\Desktop\Epoch\forestbiomass\data\test_agbm'
+
+    # Get patches of test data
+    test_patches = get_patch_names('test_patch_names')
+    predict_datagen = DataGeneratorPredict(test_patches)
+    # Load weights in of the best checkpoint
+    model.load_weights(checkpoint_filepath)
+    predictions = model.predict(predict_datagen, workers=20)
+    # Save tif files to directory to make a prediction
+    for i in range(len(test_patches)):
+        test_agbm_path = osp.join(submission_path, f'{test_patches[i]}_agbm.tif')
+        pred = predictions[i].reshape(256, 256)
+        im = Image.fromarray(pred)
+        im.save(test_agbm_path)
 
 if __name__ == '__main__':
+    # mirrored_strategy = tf.distribute.MirroredStrategy()
+
     # Change this to some pretrained cnn of keras
     # Check this link for different models: https://www.tensorflow.org/api_docs/python/tf/keras/applications
+    # strategy = tf.distribute.MultiWorkerMirroredStrategy()
+    # with strategy.scope():
     base_model = tf.keras.applications.resnet50.ResNet50(include_top=False, weights=None, input_shape=(256, 256, 11))
     base_weights = tf.keras.applications.resnet50.ResNet50(include_top=False, weights='imagenet', input_shape=(256, 256, 3))
     # Create model
+
     model = create_model(base_model, base_weights)
-    # Get patch names to feed in data generator.
-    # model = create_model_test(base_weights)
-    patch_names = get_patch_names()
+# Get patch names to feed in data generator.
+    patch_names = get_patch_names('patch_names')
     datagen = DataGeneratorNpy(patch_names)
-    checkpoint_filepath = '/tmp/checkpoint'
+
+    # # Save checkpoints
+    checkpoint_filepath = './tmp/checkpoint.hdf5'
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
         save_weights_only=True,
-        monitor='val_accuracy',
-        mode='max',
+        monitor='loss',
+        mode='min',
         save_best_only=True)
 
-    # Use data generator to fit on model
-    model.fit(datagen, epochs=10, callbacks=[model_checkpoint_callback])
+    # # Use data generator to fit on model
+    model.compile(optimizer="adam", loss=tf.keras.losses.MeanSquaredError(),
+                  metrics=[tf.keras.metrics.RootMeanSquaredError()])
+    model.fit(datagen, epochs=1, verbose=1, callbacks=[model_checkpoint_callback], max_queue_size=25, workers=50, use_multiprocessing=False)
+    # create_submissions()
