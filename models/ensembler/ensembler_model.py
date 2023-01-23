97e449ddcce41885f3215d8cf299326642606271
ptempelman <tempelmanphilip@gmail.com>
2023-01-23 11:46:39 +0100

    126 elaborate ensemble method (#127)
    * add clean chained_segmenter batch script
    
    * refactor and add ensembler setup
    
    * add framework for extracting metadata for ensemble
    
    * fix sbatch script packages
    
    * take out export python path from sbatch
    
    * add create_mask for extracting metadata
    
    * create traineval loop for ensembler
    
    * fix bug and print traineval ensembler
    
    * clean up

--- models/ensembler/ensembler_model.py
+++ models/ensembler/ensembler_model.py
@@ -5,60 +5,160 @@
 # prediction =
 # 	- x1 * (inference swin_transformer) + x2 * (inference segmenter) + x3 *  (inference pixel_wise)
 # 	- then calculate and backpropagate model over ensemble_model
+import numpy as np
 import torch
 from torch import nn
-import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
+from models.ensembler.extract_metadata import extract_metadata_batch
+from models.ensembler.universal_dataloader import UniversalDatasetTrain
 from models.utils.loss_functions import rmse_loss
 
-metadata_dim = 180
-n_hidden = 180
-weights_dim = 3
-batch_size = 32
-learning_rate = 1e-4
-image_size = 256
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-input = torch.randn(batch_size, metadata_dim)
-label = (torch.rand(size=(image_size, image_size))).float()
 
-metadata_model = nn.Sequential(nn.Linear(metadata_dim, 2 * n_hidden),
-                               nn.ReLU(),
-                               nn.Linear(2 * n_hidden, n_hidden),
-                               nn.ReLU(),
-                               nn.Linear(n_hidden, weights_dim),
-                               nn.Softmax(0))
+def bar(curr_step, total_steps, bar_length=30):
+    """
+    Printing progress bar.
+    """
+    filled = int(bar_length * curr_step / total_steps)
+    return f"[{'#' * filled}{'.' * (bar_length - filled)}]"
 
-loss_function = rmse_loss
-optimizer = torch.optim.SGD(metadata_model.parameters(), lr=learning_rate)
-epochs = 100
 
-### Here, trained models should be loaded. Model-specific data preparation also needs to be done.
-placeholder = (lambda x: torch.rand((batch_size, image_size, image_size)))
+def train_and_eval(model, train_loader, valid_loader, optimizer, criterion, epochs=10, log_freq=100):
+    """
+    Train and evaluate the model.
 
-segmentation_model = placeholder
-swin_model = placeholder
-pixelwise_model = placeholder
-###
+    @param model: (torch.nn.Module) model to train
+    @param train_loader: (torch.utils.data.DataLoader) train dataloader
+    @param valid_loader: (torch.utils.data.DataLoader) validation dataloader
+    @param optimizer: (torch.optim) optimizer
+    @param criterion: (torch.nn) loss function
+    @param epochs: (int) number of epochs
+    @return: (list, list) train loss and validation loss
+    """
+    global_train_loss, global_valid_loss = [], []
+    best_train_loss, best_valid_loss = 100, 100
+
+    for epoch in range(epochs):
+        # Training loop
+        model.train()
+        train_loss = []
+        for step, (features, agbm) in enumerate(train_loader):
+            # ======== TRAINING ========
+            metadata = extract_metadata_batch(features)
+
+            # 1. Move the tensors to the configured device.
+            # metadata, agbm, features = metadata.to(DEVICE), agbm.to(DEVICE), features.to(DEVICE)
+            # 2. Forward pass by passing the images through the model.
+            w1, w2, w3 = torch.tensor_split(model(metadata), 3, dim=1)
+            w1, w2, w3 = w1.view(-1, 1, 1), w2.view(-1, 1, 1), w3.view(-1, 1, 1)
+            pred = (w1 * segmentation_model(features)) + (w2 * swin_model(features)) + (w3 * pixelwise_model(features))
+
+            # 3. Zero the gradients of all model parameters.
+            optimizer.zero_grad()
+            # 4. Compute the loss.
+            loss = criterion(pred, agbm)
+            # 5. Backward pass to compute the gradients of the loss w.r.t. the model parameters.
+            loss.backward()
+            # 6. Step the optimizer to update the model parameters.
+            optimizer.step()
+            # =========================
+
+            # Logging metrics
+            train_loss.append(loss.item())
+
+            if step % log_freq == 0 or step == len(train_loader) - 1:
+                print(
+                    f"Epoch {epoch + 1:2d}/{epochs:2d} {bar(step + 1, len(train_loader))} Train-Loss: {np.mean(train_loss):.4f} \r",
+                    end="")
+        print()
+
+        # Evaluation loop
+        model.eval()
+        valid_loss = []
+        with torch.no_grad():
+            for step, (features, agbm) in enumerate(valid_loader):
+                metadata = extract_metadata_batch(features)
+
+                # ======== VALIDATION ========
+                # 1. Move the tensors to the configured device.
+                # metadata, agbm, features = metadata.to(DEVICE), agbm.to(DEVICE), features.to(DEVICE)
+                # 2. Forward pass by passing the images through the model.
+                w1, w2, w3 = torch.tensor_split(model(metadata), 3, dim=1)
+                w1, w2, w3 = w1.view(-1, 1, 1), w2.view(-1, 1, 1), w3.view(-1, 1, 1)
+                pred = (w1 * segmentation_model(features)) + (w2 * swin_model(features)) + (w3 * pixelwise_model(features))
+
+                # 3. Compute the loss.
+                loss = criterion(pred, agbm)
+                # Note we do not backpropagate the gradients in the validation loop.
+                # ==========================
+
+                # Logging metrics
+                valid_loss.append(loss.item())
+
+                if step % log_freq == 0 or step == len(valid_loader) - 1:
+                    print(
+                        f"Epoch {epoch + 1:2d}/{epochs:2d} {bar(step + 1, len(valid_loader))} Valid-Loss: {np.mean(valid_loss):.4f} \r",
+                        end="")
+            print()
+
+        global_train_loss.extend(train_loss)
+        global_valid_loss.extend(valid_loss)
+        best_train_loss = min(best_train_loss, np.mean(train_loss))
+        best_valid_loss = min(best_valid_loss, np.mean(valid_loss))
+        print(f"Best Train Loss: {best_train_loss} Best Valid Loss: {best_valid_loss}")
+
+    return global_train_loss, global_valid_loss
+
+
+def create_metadata_model(metadata_dim, n_hidden, weights_dim):
+    return nn.Sequential(nn.Linear(metadata_dim, 2 * n_hidden),
+                         nn.ReLU(),
+                         nn.Linear(2 * n_hidden, n_hidden),
+                         nn.ReLU(),
+                         nn.Linear(n_hidden, weights_dim),
+                         nn.Softmax(0))
+
+
+def create_train_val_dataloaders(num_workers):
+    dataset = UniversalDatasetTrain()
+    train_size = int(0.8 * len(dataset))
+    valid_size = len(dataset) - train_size
+
+    dataset_train, dataset_val = torch.utils.data.random_split(dataset, [train_size, valid_size])
+
+    dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+    return dataloader_train, dataloader_val
+
 
 if __name__ == '__main__':
+    metadata_dim = 180
+    n_hidden = 180
+    weights_dim = 3
+    learning_rate = 1e-4
+    metadata_model = create_metadata_model(metadata_dim=metadata_dim, n_hidden=n_hidden, weights_dim=weights_dim)
+    optimizer = torch.optim.SGD(metadata_model.parameters(), lr=learning_rate)
+    loss_function = rmse_loss
+    epochs = 100
 
-    losses = []
-    for epoch in range(epochs):
-        w1, w2, w3 = torch.tensor_split(metadata_model(input), weights_dim, dim=1)
+    batch_size = 16
+    image_size = 256
+    placeholder = (lambda x: torch.rand((x.shape[0], image_size, image_size)))
+    # TODO: Placeholders should be replaced by a method that takes a batch and returns a batch of predictions.
+    #  To make the predictions a pretrained model should be loaded.
+    segmentation_model = placeholder
+    swin_model = placeholder
+    pixelwise_model = placeholder
 
-        w1, w2, w3 = w1.view(-1, 1, 1), w2.view(-1, 1, 1), w3.view(-1, 1, 1)
-        pred_y = (w1 * segmentation_model(input)) + (w2 * swin_model(input)) + (w3 * pixelwise_model(input))
+    train_loader, valid_loader = create_train_val_dataloaders(num_workers=20)
 
-        loss = loss_function(pred_y, label)
-        losses.append(loss.item())
-
-        metadata_model.zero_grad()
-        loss.backward()
-
-        optimizer.step()
-
-    plt.plot(losses)
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.title("Learning rate %f" % learning_rate)
-    plt.show()
+    train_and_eval(model=metadata_model,
+                   train_loader=train_loader,
+                   valid_loader=valid_loader,
+                   optimizer=optimizer,
+                   criterion=loss_function,
+                   epochs=epochs, log_freq=100)
--- models/ensembler/extract_metadata.py
+++ models/ensembler/extract_metadata.py
@@ -7,25 +7,49 @@
 # 	- missing = 1
 
 import torch
+from torch.nn import functional as F
+
+
+def create_mask(image_tensor, radius):
+    assert image_tensor.shape == (256, 256), "Input image must be of shape (256, 256)"
+    # add singleton dimension
+    image_tensor = image_tensor.unsqueeze(0)
+    # Create convolution mask
+    convolution_mask = F.avg_pool2d(image_tensor, kernel_size=radius * 2 + 1, stride=1, padding=radius)
+    # remove singleton dimension
+    convolution_mask = convolution_mask.squeeze(0)
+    # Create comparison mask
+    comparison_mask = torch.where(image_tensor == convolution_mask, torch.tensor(0.), torch.tensor(1.))
+    return comparison_mask[0]
 
 
 def extract_metadata_band(band, missing):
     if missing:
-        return 1
+        return 0
     else:
-        # return the percentage of pixels that are the same as half of their closest 50 neighbours
-        # for edge pixels the threshold should be increased according to how close they are to the edge
-        pass
+        return torch.mean(create_mask(band, radius=2))
 
 
 def extract_metadata_batch(batch):
+    """
+    @param batch: A torch tensor of shape (batch_size, 12 * num_bands, 256, 256)
+    @return: A torch tensor of shape (batch_size, 12 * num_bands) indicating "corruptedness" for each band, where 0 is very corrupted
+    and 1 is not corrupted
+    """
     batch_metadata = []
     for patch in batch:
 
-        # TODO: see how missing bands are treated in the dataloader we want to use and change code accordingly
         patch_metadata = []
         for band in patch:
             patch_metadata.append(extract_metadata_band(band, False))
 
         batch_metadata.append(patch_metadata)
     return torch.FloatTensor(batch_metadata)
+
+
+if __name__ == '__main__':
+    # very_corrupted = torch.zeros(16, 180, 256, 256)
+    # print(extract_metadata_batch(very_corrupted))
+
+    non_corrupted = torch.rand(16, 180, 256, 256)
+    print(extract_metadata_batch(non_corrupted).shape)
